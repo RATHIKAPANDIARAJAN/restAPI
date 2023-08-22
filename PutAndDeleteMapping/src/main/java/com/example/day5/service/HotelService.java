b28fd9ecc4ad24104750afccc29c010645f5e622
RATHIKAPANDIARAJAN <142587131+RATHIKAPANDIARAJAN@users.noreply.github.com>
2023-08-22 16:07:18 +0530

    Add files via upload

---
+++ PutAndDeleteMapping/src/main/java/com/example/day5/service/HotelService.java
@@ -0,0 +1,60 @@
+package com.example.day5.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.day5.model.HotelModel;
+import com.example.day5.repository.HotelRepo;
+
+@Service
+public class HotelService {
+    
+	@Autowired
+	public HotelRepo hrepo;
+	//post the data
+	public String saveHotel(HotelModel h)
+	{
+		hrepo.save(h);
+		return "Data is saved to database";
+	}
+	//get the data
+	public List<HotelModel> getHotel()
+	{
+		return hrepo.findAll();
+	}
+	//update the data(put )
+	public HotelModel updateHotel(HotelModel hm) {
+		return hrepo.saveAndFlush(hm);
+	}
+	public boolean updateHotelif(int hotelId,HotelModel hm) {
+		if(hrepo.existsById(hotelId)) {
+			hrepo.save(hm);
+			return true;
+		}
+		return false;
+	}
+	public boolean deleteHotelif(int hotelId) {
+		if(hrepo.existsById(hotelId)) {
+			hrepo.deleteById(hotelId);
+			return true;
+		}
+		return false;
+	}
+	public void deleteHotel(int hotel_id) {
+		System.out.println("Deleted");
+		hrepo.deleteById(hotel_id);
+	}
+	public Optional<HotelModel> getUserId(int userId){
+		Optional<HotelModel>hotel=hrepo.findById(userId);
+		if(hotel.isPresent()) {
+			return hotel;
+		}
+		return null;
+	}
+
+	
+	
+}
